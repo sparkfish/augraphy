ef14d4af174c54040393ea49b5734c508340ce79
Brett Allen <ballen@sparkfish.com>
2021-06-16 10:36:23 -0500

    Minor cleanup of testing script.

--- src/test.py
+++ src/test.py
@@ -26,41 +26,42 @@ def create_pdf417():
 img = np.array(create_pdf417())
 
 ink_phase = AugmentationSequence([
-                                  InkBleedAugmentation(),
-                                  DustyInkAugmentation(),
-                                  LowInkBlobsAugmentation(), 
-                                  OneOf([
-                                    LowInkRandomLinesAugmentation(count_range=(3, 10), use_consistent_lines=False), #line jitter, add range 
-                                    LowInkRandomLinesAugmentation(count_range=(3, 10), use_consistent_lines=True), 
-                                    LowInkPeriodicLinesAugmentation(count_range=(2, 2), period_range=(5, 5), use_consistent_lines=False), 
-                                    LowInkPeriodicLinesAugmentation(count_range=(2, 2), period_range=(5, 5), use_consistent_lines=True), 
-                                  ]),
-                                  GaussianBlurAugmentation(probability=1)
-                                ])
+    InkBleedAugmentation(),
+    DustyInkAugmentation(),
+    LowInkBlobsAugmentation(), 
+    OneOf([
+        LowInkRandomLinesAugmentation(count_range=(3, 10), use_consistent_lines=False),
+        LowInkRandomLinesAugmentation(count_range=(3, 10), use_consistent_lines=True), 
+        LowInkPeriodicLinesAugmentation(count_range=(2, 2), period_range=(5, 5), use_consistent_lines=False), 
+        LowInkPeriodicLinesAugmentation(count_range=(2, 2), period_range=(5, 5), use_consistent_lines=True), 
+    ]),
+    GaussianBlurAugmentation(probability=1)
+])
 
 paper_phase = AugmentationSequence([
-                                    OneOf([
-                                      AugmentationSequence([
-                                          NoiseTexturizeAugmentation(),
-                                          BrightnessTexturizeAugmentation()
-                                        ]),
-                                      AugmentationSequence([
-                                          BrightnessTexturizeAugmentation(probability=1.0),
-                                          NoiseTexturizeAugmentation()
-                                    ])]),
-                                    GaussianBlurAugmentation([(3,3), (3,5), (5,3), (5,5)]),
-                                    BrightnessAugmentation()
-                            ])
+    OneOf([
+        AugmentationSequence([
+            NoiseTexturizeAugmentation(),
+            BrightnessTexturizeAugmentation()
+        ]),
+        AugmentationSequence([
+            BrightnessTexturizeAugmentation(probability=1.0),
+            NoiseTexturizeAugmentation()
+        ])
+    ]),
+    GaussianBlurAugmentation([(3,3), (3,5), (5,3), (5,5)]),
+    BrightnessAugmentation()
+])
 
-
-post_phase = AugmentationSequence([DirtyRollersAugmentation(),
-                                   OneOf([
-                                        LightingGradientAugmentation(),
-                                        BrightnessAugmentation()
-                                      ]),
-                                      JpegAugmentation(), 
-                                      SubtleNoiseAugmentation()
-                                    ])
+post_phase = AugmentationSequence([
+    DirtyRollersAugmentation(),
+    OneOf([
+        LightingGradientAugmentation(),
+        BrightnessAugmentation()
+    ]),
+    JpegAugmentation(), 
+    SubtleNoiseAugmentation()
+])
 
 pipeline = AugraphyPipeline(ink_phase, paper_phase, post_phase)
 
