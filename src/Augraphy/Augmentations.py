375df4aedafddd22e766763a5dbe83739b03136f
Shaheryar <shaheryartariq909@gmail.com>
2021-07-06 22:50:54 +0500

    Added GammaAugmentation

--- src/Augraphy/Augmentations.py
+++ src/Augraphy/Augmentations.py
@@ -3,6 +3,7 @@ import math
 import cv2
 import numpy as np
 import random
+import os
 
 from glob import glob
 from scipy.stats import norm
@@ -547,6 +548,26 @@ class BrightnessAugmentation(Augmentation):
       if self.layer == "ink":
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
       data[self.layer].append(AugmentationResult(self, img))
+class GammaAugmentation(Augmentation):
+  def __init__(self, range=(0.5, 1.5), probability=0.5):
+    super().__init__(probability=probability)
+    self.range = range
+
+  def __repr__(self):
+    return f"GammaAugmentation(range={self.range}, probability={self.probability})"
+
+  def __call__(self, data,force=False):
+    img = data['post'][-1].result
+    img = img.astype(np.uint8)
+    value = random.uniform(self.range[0], self.range[1])
+    invGamma = 1.0 / value
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
+    print(table.shape,img.shape)
+    frame= cv2.LUT(img, table)
+    data['post'].append(AugmentationResult(self, frame))
+    cv2.imwrite(os.path.join('test_outputs',str(round(value,4))+"gamma.jpg"),np.hstack((img,frame)))
+
 
 class LightingGradientAugmentation(Augmentation):
   """
