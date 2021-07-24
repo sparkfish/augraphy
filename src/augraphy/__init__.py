8692881ddefa99e03591a86548737582b7a64183
alex <source@proof.construction>
2021-07-24 10:59:36 +0800

    add init files for more convenient imports

---
+++ src/augraphy/__init__.py
@@ -0,0 +1,4 @@
+from augraphy.base import *
+from augraphy.augmentations import *
+from augraphy.default import *
+from augraphy.utilities import *
---
+++ src/augraphy/augmentations/__init__.py
@@ -0,0 +1,39 @@
+from augraphy.augmentations.bleedthrough import BleedThroughAugmentation
+from augraphy.augmentations.brightness import BrightnessAugmentation
+from augraphy.augmentations.brightnesstexturize import BrightnessTexturizeAugmentation
+from augraphy.augmentations.dirtydrum import DirtyDrumAugmentation
+from augraphy.augmentations.dirtyrollers import DirtyRollersAugmentation
+from augraphy.augmentations.dustyink import DustyInkAugmentation
+from augraphy.augmentations.folding import FoldingAugmentation
+from augraphy.augmentations.gamma import GammaAugmentation
+from augraphy.augmentations.gaussianblur import GaussianBlurAugmentation
+from augraphy.augmentations.inkbleed import InkBleedAugmentation
+from augraphy.augmentations.jpeg import JpegAugmentation
+from augraphy.augmentations.letterpress import LetterpressAugmentation
+from augraphy.augmentations.lightinggradient import LightingGradientAugmentation
+from augraphy.augmentations.lowinkblobs import LowInkBlobsAugmentation
+from augraphy.augmentations.lowinkperiodiclines import LowInkPeriodicLinesAugmentation
+from augraphy.augmentations.lowinkrandomlines import LowInkRandomLinesAugmentation
+from augraphy.augmentations.noisetexturize import NoiseTexturizeAugmentation
+from augraphy.augmentations.subtlenoise import SubtleNoiseAugmentation
+
+__all__ = [
+    "BleedThroughAugmentation",
+    "BrightnessAugmentation",
+    "BrightnessTexturizeAugmentation",
+    "DirtyDrumAugmentation",
+    "DirtyRollersAugmentation",
+    "DustyInkAugmentation",
+    "FoldingAugmentation",
+    "GammaAugmentation",
+    "GaussianBlurAugmentation",
+    "InkBleedAugmentation",
+    "JpegAugmentation",
+    "LetterpressAugmentation",
+    "LightingGradientAugmentation",
+    "LowInkBlobsAugmentation",
+    "LowInkPeriodicLinesAugmentation",
+    "LowInkRandomLinesAugmentation",
+    "NoiseTexturizeAugmentation",
+    "SubtleNoiseAugmentation"
+    ]
