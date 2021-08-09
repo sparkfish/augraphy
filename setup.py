96baf053c45d6305a6163e4baad5473257f8b07a
alex <source@proof.construction>
2021-08-09 12:47:21 +0800

    remove nested package directory

--- setup.py
+++ setup.py
@@ -20,8 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.7",
     install_requires=[
         "numpy >= 1.20.1",
