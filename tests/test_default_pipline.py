cce09c47fa2c3a4e09595ea79bba05ac96d2d043
proofconstruction <74747193+proofconstruction@users.noreply.github.com>
2021-08-21 16:53:15 +0800

    Update test_default_pipline.py

--- tests/test_default_pipline.py
+++ tests/test_default_pipline.py
@@ -7,9 +7,9 @@ from augraphy import *
 
 @pytest.fixture
 def random_image():
-    xdim = random.randint(1, 500)
-    ydim = random.randint(1, 500)
-    return np.random.randint(low=0, high=256, size=(xdim, ydim, 3), dtype=np.uint8)
+    xdim = random.randint(30, 500)
+    ydim = random.randint(30, 500)
+    return np.random.randint(low=0, high=255, size=(xdim, ydim, 3), dtype=np.uint8)
 
 
 @pytest.fixture
