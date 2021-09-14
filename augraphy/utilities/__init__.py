f7b765aa80d4c6d1844fa166adc23515cb9afbb5
alex <source@proof.construction>
2021-09-14 14:39:51 +0000

    fix overlaybuilder import

--- augraphy/utilities/__init__.py
+++ augraphy/utilities/__init__.py
@@ -2,10 +2,12 @@ from augraphy.utilities.composepipelines import ComposePipelines
 from augraphy.utilities.figsharedownloader import FigshareDownloader
 from augraphy.utilities.foreign import ForeignAugmentation
 from augraphy.utilities.imageoverlay import ImageOverlay
+from augraphy.utilities.overlaybuilder import OverlayBuilder
 
 __all__ = [
     "ComposePipelines",
     "ForeignAugmentation",
     "FigshareDownloader",
     "ImageOverlay",
+    "OverlayBuiler",
 ]
