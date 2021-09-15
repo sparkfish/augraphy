f80cfb561d8ca61415e74814d5b020ed63900bcc
alex <source@proof.construction>
2021-09-15 14:31:02 +0000

    add arbitrary functions wrapper utility augmentation

--- augraphy/utilities/__init__.py
+++ augraphy/utilities/__init__.py
@@ -1,6 +1,7 @@
 from augraphy.utilities.composepipelines import ComposePipelines
 from augraphy.utilities.figsharedownloader import FigshareDownloader
 from augraphy.utilities.foreign import ForeignAugmentation
+from augraphy.utilities.function import Function
 from augraphy.utilities.imageoverlay import ImageOverlay
 from augraphy.utilities.overlaybuilder import OverlayBuilder
 
@@ -8,6 +9,7 @@ __all__ = [
     "ComposePipelines",
     "ForeignAugmentation",
     "FigshareDownloader",
+    "Function",
     "ImageOverlay",
     "OverlayBuilder",
 ]
