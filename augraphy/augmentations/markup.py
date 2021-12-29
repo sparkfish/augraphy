5dd52fdd3b497312606c6d3afa4003f94a8cbcc4
Shaheryar <shaheryartariq909@gmail.com>
2021-12-29 16:51:35 +0500

    Added crossed-off style in markup

--- augraphy/augmentations/markup.py
+++ augraphy/augmentations/markup.py
@@ -21,7 +21,7 @@ class Markup(Augmentation):
     :type markup_length_range: float tuple, optional
     :param markup_thickness_range: Pair of ints, to determine the thickness of added markup effect.
     :type markup_thickness_range: int tuple, optional
-    :param markup_type: Choice of markup "strikethrough", "highlight" or "underline".
+    :param markup_type: Choice of markup "strikethrough", "highlight", "underline" or "crossed".
     :type markup_type: string
     :param markup_color: bgr color tuple.
     :type markup_color: tuple of ints
@@ -42,8 +42,8 @@ class Markup(Augmentation):
         markup_thickness_range=(1, 3),
         markup_type="strikethrough",
         markup_color=(0, 255, 0),
-        repetitions=1,
         single_word_mode=False,
+        repetitions=1,
         p=1,
     ):
 
@@ -62,14 +62,21 @@ class Markup(Augmentation):
             f"markup_thickness_range={self.markup_thickness_range},  markup_type{self. markup_type} p={self.p})"
         )
 
-    def __call__(self, image, layer=None, force=False):
-        markup_img = image.copy()
-        overlay = markup_img.copy()
-        num_lines = random.randint(self.num_lines_range[0], self.num_lines_range[1])
-        markup_thickness = random.randint(
-            self.markup_thickness_range[0],
-            self.markup_thickness_range[1],
-        )
+    def distribute_line(self, starting_point, ending_point, offset):
+        points_count = random.randint(3, 10)  # dividing the line into points
+        points = np.linspace(starting_point[0], ending_point[0], points_count)
+        points = [[int(x), (starting_point[1] + random.randint(-offset, offset))] for x in points]
+        points = smooth(points, 6)  # adding a smoothing effect in points using chaikin's algorithm
+        return points
+
+    def apply_crossed_off(self, image, p1, p2, offset):
+        apply_random_offset_fn = lambda p, offset: p + random.randint(-offset, offset)
+        apply_random_offset = np.vectorize(apply_random_offset_fn)
+        p1 = tuple(apply_random_offset(p1, offset))
+        p2 = tuple(apply_random_offset(p2, offset))
+        drawn = cv2.line(image, p1, p2, self.markup_color, self.mar, lineType=cv2.LINE_AA)
+
+    def _preprocess(self, image):
         blurred = cv2.blur(image, (5, 5))
         blurred = blurred.astype("uint8")
         if len(blurred.shape) > 2 and blurred.shape[2] == 3:
@@ -101,10 +108,22 @@ class Markup(Augmentation):
             None,
             iterations=1,
         )
+        return dilation
+
+    def __call__(self, image, layer=None, force=False):
+        markup_img = image.copy()
+        overlay = markup_img.copy()
+        num_lines = random.randint(self.num_lines_range[0], self.num_lines_range[1])
+        markup_thickness = random.randint(
+            self.markup_thickness_range[0],
+            self.markup_thickness_range[1],
+        )
+
+        binary_image = self._preprocess(image)
 
         # Applying dilate operation to connect text lines horizontaly.
         contours, hierarchy = cv2.findContours(
-            dilation,
+            binary_image,
             cv2.RETR_EXTERNAL,
             cv2.CHAIN_APPROX_NONE,
         )  # Each line is detected as a contour.
@@ -122,7 +141,6 @@ class Markup(Augmentation):
                     continue
                 if num_lines == 0:
                     break
-
                 num_lines = num_lines - 1
                 markup_length = random.uniform(
                     self.markup_length_range[0],
@@ -139,39 +157,72 @@ class Markup(Augmentation):
                 if self.markup_type == "strikethrough":
                     starting_point = [x, int(y + (h / 2))]
                     ending_point = [x + w, int(y + (h / 2))]
+                # for corossed-off we need points representing primary diagonal
+                elif self.markup_type == "crossed":
+                    starting_point = [x, y]
+                    ending_point = [x + w, y + h]
                 else:
                     # for underline and highlight we need points corresponding to bottom part of text
                     starting_point = [x, y + h]
                     ending_point = [x + w, y + h]
 
-                # Generating markup repetition points
-                all_points_lists = []
                 for i in range(self.repetitions):
-                    # # dividing the line into points
-                    points_count = random.randint(3, 10)  # dividing the line into points
-                    points = np.linspace(starting_point[0], ending_point[0], points_count)
-                    points = [[int(x), (starting_point[1] + random.randint(-offset, offset))] for x in points]
-                    points = smooth(points, 6)  # adding a smoothing effect in points using chaikin's algorithm
-                    all_points_lists.append(points)
+                    if self.markup_type == "crossed":
+                        # Drawing primary diagonal
+                        p1 = (
+                            starting_point[0] + random.randint(-offset, offset),
+                            starting_point[1] + +random.randint(-offset, offset),
+                        )
+                        p2 = (
+                            ending_point[0] + random.randint(-offset, offset),
+                            ending_point[1] + +random.randint(-offset, offset),
+                        )
+                        markup_mask = cv2.line(
+                            markup_mask,
+                            p1,
+                            p2,
+                            self.markup_color,
+                            markup_thickness,
+                            lineType=cv2.LINE_AA,
+                        )
+                        # drawing secondary diagonal
+                        p1 = (
+                            ending_point[0] + random.randint(-offset, offset),
+                            starting_point[1] + +random.randint(-offset, offset),
+                        )
+                        p2 = (
+                            starting_point[0] + random.randint(-offset, offset),
+                            ending_point[1] + +random.randint(-offset, offset),
+                        )
+                        markup_mask = cv2.line(
+                            markup_mask,
+                            p1,
+                            p2,
+                            self.markup_color,
+                            markup_thickness,
+                            lineType=cv2.LINE_AA,
+                        )
 
-                for k in range(len(all_points_lists)):
-                    points_list = all_points_lists[k]
-                    for i in range(len(points_list) - 1):
-                        p1 = (int(points_list[i][0]), int(points_list[i][1]))
-                        if self.markup_type == "highlight":
-                            p2 = (int(points_list[i + 1][0]), int(points_list[i + 1][1] - h))
-                            # A filled rectangle
-                            markup_mask = cv2.rectangle(markup_mask, p1, p2, self.markup_color, -1)
-                        else:
-                            p2 = (int(points_list[i + 1][0]), int(points_list[i + 1][1]))
-                            markup_mask = cv2.line(
-                                markup_mask,
-                                p1,
-                                p2,
-                                self.markup_color,
-                                markup_thickness,
-                                lineType=cv2.LINE_AA,
-                            )
+                    else:
+                        # dividing the line into points to mimic a smoothing effect
+                        points_list = self.distribute_line(starting_point, ending_point, offset)
+                        for i in range(len(points_list) - 1):
+                            p1 = (int(points_list[i][0]), int(points_list[i][1]))
+                            if self.markup_type == "highlight":
+                                p2 = (int(points_list[i + 1][0]), int(points_list[i + 1][1] - h))
+                                # A filled rectangle
+                                markup_mask = cv2.rectangle(markup_mask, p1, p2, self.markup_color, -1)
+
+                            else:
+                                p2 = (int(points_list[i + 1][0]), int(points_list[i + 1][1]))
+                                markup_mask = cv2.line(
+                                    markup_mask,
+                                    p1,
+                                    p2,
+                                    self.markup_color,
+                                    markup_thickness,
+                                    lineType=cv2.LINE_AA,
+                                )
 
         # smoothen highlight mask to make it more realistic
         if self.markup_type == "highlight":
