044af98c169e7d1cda36bfcbe23443941b8261a3
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2023-06-03 21:02:34 +0800

    Rerun pre-commit.

--- augraphy/augmentations/colorshift.py
+++ augraphy/augmentations/colorshift.py
@@ -11,12 +11,12 @@ class ColorShift(Augmentation):
 
     :param color_shift_offset_x_range: Pair of ints/floats determining the value of x offset in shifting each color channel.
             If the value is within the range of 0.0 to 1.0 and the value is float,
-            the x offset will be scaled by image width: 
+            the x offset will be scaled by image width:
             x offset (int) = image width  * x offset (float and 0.0 - 1.0)
-    :type color_shift_offset_x_range: tuple, optional   
+    :type color_shift_offset_x_range: tuple, optional
     :param color_shift_offset_y_range: Pair of ints/floats determining the value of y offset in shifting each color channel.
             If the value is within the range of 0.0 to 1.0 and the value is float,
-            the y offset will be scaled by image height: 
+            the y offset will be scaled by image height:
             y offset (int) = image height  * y offset (float and 0.0 - 1.0)
     :type color_shift_offset_y_range: tuple, optional
     :param color_shift_iterations: Pair of ints determining the number of iterations in applying the color shift operation.
@@ -32,11 +32,11 @@ class ColorShift(Augmentation):
 
     def __init__(
         self,
-        color_shift_offset_x_range = (3,5),
-        color_shift_offset_y_range = (3,5),
-        color_shift_iterations = (1,2),
-        color_shift_brightness_range = (0.9,1.1),
-        color_shift_gaussian_kernel_range = (3,3),
+        color_shift_offset_x_range=(3, 5),
+        color_shift_offset_y_range=(3, 5),
+        color_shift_iterations=(1, 2),
+        color_shift_brightness_range=(0.9, 1.1),
+        color_shift_gaussian_kernel_range=(3, 3),
         p=1,
     ):
         """Constructor method"""
@@ -51,106 +51,113 @@ class ColorShift(Augmentation):
         return f"ColorShift(color_shift_offset_x_range={self.color_shift_offset_x_range}, color_shift_offset_y_range={self.color_shift_offset_y_range}, color_shift_iterations={self.color_shift_iterations}, color_shift_brightness_range={self.color_shift_brightness_range}, color_shift_gaussian_kernel_range={self.color_shift_gaussian_kernel_range}, p={self.p})"
 
     def apply_color_shift(self, image):
-        """ Main function to apply color shift process.
-        
+        """Main function to apply color shift process.
+
         :param image: The input image.
         :type image: numpy array
         """
-        
+
         ysize, xsize = image.shape[:2]
-            
+
         image_b, image_g, image_r = cv2.split(image.copy())
         images = [image_b, image_g, image_r]
-            
+
         brightness_ratio = random.uniform(self.color_shift_brightness_range[0], self.color_shift_brightness_range[1])
-        
+
         # possible combinations in term of x and y direction
-        directions = [[1,1], [1,-1], [-1,1], [-1,-1]]
+        directions = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
         for i, image_single_color in enumerate(images):
-            
+
             # get random direction
-            index = random.randint(0, len(directions)-1)
+            index = random.randint(0, len(directions) - 1)
             direction_x, direction_y = directions.pop(index)
-            
+
             # generate random offsets
-            if self.color_shift_offset_x_range[0] <=1.0 and isinstance(self.color_shift_offset_x_range[0], float):
-                offset_x = random.randint(int(self.color_shift_offset_x_range[0]* xsize),int(self.color_shift_offset_x_range[1]* xsize))
+            if self.color_shift_offset_x_range[0] <= 1.0 and isinstance(self.color_shift_offset_x_range[0], float):
+                offset_x = random.randint(
+                    int(self.color_shift_offset_x_range[0] * xsize),
+                    int(self.color_shift_offset_x_range[1] * xsize),
+                )
             else:
-                offset_x = random.randint(self.color_shift_offset_x_range[0],  self.color_shift_offset_x_range[1])
-            if self.color_shift_offset_y_range[0] <=1.0 and isinstance(self.color_shift_offset_y_range[0], float):
-                offset_y = random.randint(int(self.color_shift_offset_y_range[0]* ysize),int(self.color_shift_offset_y_range[1]* ysize))
+                offset_x = random.randint(self.color_shift_offset_x_range[0], self.color_shift_offset_x_range[1])
+            if self.color_shift_offset_y_range[0] <= 1.0 and isinstance(self.color_shift_offset_y_range[0], float):
+                offset_y = random.randint(
+                    int(self.color_shift_offset_y_range[0] * ysize),
+                    int(self.color_shift_offset_y_range[1] * ysize),
+                )
             else:
-                offset_y = random.randint(self.color_shift_offset_y_range[0],  self.color_shift_offset_y_range[1])
+                offset_y = random.randint(self.color_shift_offset_y_range[0], self.color_shift_offset_y_range[1])
 
             # y direction
-            translation_matrix_y = np.float32([[1, 0, 0], [0, 1, offset_y* direction_y]])
+            translation_matrix_y = np.float32([[1, 0, 0], [0, 1, offset_y * direction_y]])
             # get a copy of translated area
-            if direction_y>0:
+            if direction_y > 0:
                 image_patch = image_single_color[-offset_y:, :].copy()
-            else:   
+            else:
                 image_patch = image_single_color[:offset_y, :].copy()
             # shift image in y direction
             image_single_color = cv2.warpAffine(image_single_color, translation_matrix_y, (xsize, ysize))
             # fill back the empty area after translation
-            if direction_y>0:
+            if direction_y > 0:
                 image_single_color[:offset_y, :] = image_patch
-            else:   
+            else:
                 image_single_color[-offset_y:, :] = image_patch
-            
+
             # x direction
             translation_matrix_x = np.float32([[1, 0, offset_x * direction_x], [0, 1, 0]])
             # get a copy of translated area
-            if direction_x>0:
+            if direction_x > 0:
                 image_patch = image_single_color[:, -offset_x:].copy()
-            else:   
+            else:
                 image_patch = image_single_color[:, :offset_x].copy()
             # shift image in x direction
             image_single_color = cv2.warpAffine(image_single_color, translation_matrix_x, (xsize, ysize))
             # fill back the empty area after translation
-            if direction_x>0:
+            if direction_x > 0:
                 image_single_color[:, :offset_x] = image_patch
-            else:   
+            else:
                 image_single_color[:, -offset_x:] = image_patch
 
             # apply random brightness
-            image_single_color_ratio = image_single_color.astype("int")* brightness_ratio
-            image_single_color_ratio[image_single_color_ratio>255] = 255
-            image_single_color_ratio[image_single_color_ratio<0] = 0
+            image_single_color_ratio = image_single_color.astype("int") * brightness_ratio
+            image_single_color_ratio[image_single_color_ratio > 255] = 255
+            image_single_color_ratio[image_single_color_ratio < 0] = 0
             image_single_color_ratio = image_single_color_ratio.astype("uint8")
-            
+
             # blur the shhifted image
-            kernel_value = random.randint(self.color_shift_gaussian_kernel_range[0], self.color_shift_gaussian_kernel_range[1])
+            kernel_value = random.randint(
+                self.color_shift_gaussian_kernel_range[0],
+                self.color_shift_gaussian_kernel_range[1],
+            )
             # kernel must be odd
             if not (kernel_value % 2):
                 kernel_value += 1
-            image_single_color_ratio = cv2.GaussianBlur(image_single_color_ratio, (kernel_value,kernel_value),0)
-            
+            image_single_color_ratio = cv2.GaussianBlur(image_single_color_ratio, (kernel_value, kernel_value), 0)
+
             # reassign the shifted color channel back to the image
-            image[:,:,i] = image_single_color_ratio
+            image[:, :, i] = image_single_color_ratio
 
         return image
-        
-        
+
     def __call__(self, image, layer=None, force=False):
         if force or self.should_run():
-            
-            
+
             image_output = image.copy()
-  
+
             # convert and make sure image is color image
             if len(image_output.shape) > 2:
                 is_gray = 0
             else:
                 is_gray = 1
                 image_output = cv2.cvtColor(image_output, cv2.COLOR_GRAY2BGR)
-    
+
             # apply color shift based on the generated iterations
             color_shift_iterations = random.randint(self.color_shift_iterations[0], self.color_shift_iterations[1])
             for i in range(color_shift_iterations):
                 image_output = self.apply_color_shift(image_output)
-                
+
             # return image follows the input image color channel
             if is_gray:
                 image_output = cv2.cvtColor(image_output, cv2.COLOR_BGR2GRAY)
-                
+
             return image_output
--- augraphy/augmentations/glitcheffect.py
+++ augraphy/augmentations/glitcheffect.py
@@ -3,8 +3,8 @@ import random
 import cv2
 import numpy as np
 
-from augraphy.base.augmentation import Augmentation
 from augraphy.augmentations.colorshift import ColorShift
+from augraphy.base.augmentation import Augmentation
 
 
 class GlitchEffect(Augmentation):
@@ -16,12 +16,12 @@ class GlitchEffect(Augmentation):
     :type glitch_number_range: tuple, optional
     :param glitch_size_range: Tuple of ints/floats determing the size of image patches.
             If the value is within the range of 0.0 to 1.0 and the value is float,
-            the size will be scaled by image height: 
+            the size will be scaled by image height:
             size (int) = image height  * size (float and 0.0 - 1.0)
     :type glitch_size_range: tuple, optional
     :param glitch_offset_range: Tuple of ints/floats determing the offset value to shift the image patches.
             If the value is within the range of 0.0 to 1.0 and the value is float,
-            the size will be scaled by image width: 
+            the size will be scaled by image width:
             offset (int) = image width  * offset (float and 0.0 - 1.0)
     :type glitch_offset_range: tuple, optional
     :param p: The probability that this Augmentation will be applied.
@@ -30,10 +30,10 @@ class GlitchEffect(Augmentation):
 
     def __init__(
         self,
-        glitch_direction = "random",
-        glitch_number_range = (8, 16),
-        glitch_size_range = (5, 50),
-        glitch_offset_range = (10, 50),
+        glitch_direction="random",
+        glitch_number_range=(8, 16),
+        glitch_size_range=(5, 50),
+        glitch_offset_range=(10, 50),
         p=1,
     ):
         """Constructor method"""
@@ -42,14 +42,13 @@ class GlitchEffect(Augmentation):
         self.glitch_number_range = glitch_number_range
         self.glitch_size_range = glitch_size_range
         self.glitch_offset_range = glitch_offset_range
-        
 
     def __repr__(self):
         return f"GlitchEffect(glitch_direction={self.glitch_direction}, glitch_number_range={self.glitch_number_range}, glitch_size_range={self.glitch_size_range}, glitch_offset_range={self.glitch_offset_range}, p={self.p})"
 
     def apply_glitch(self, image):
         """Apply glitch effect into the image by shifting patches of images.
-        
+
         :param image: Image to apply the glitch effect.
         :type image: numpy array
         """
@@ -59,65 +58,72 @@ class GlitchEffect(Augmentation):
         for i in range(glitch_number):
 
             # generate random glitch size
-            if self.glitch_size_range[0] <=1.0 and isinstance(self.glitch_size_range[0], float):
-                glitch_size = random.randint(int(self.glitch_size_range[0]*ysize), int(self.glitch_size_range[1]*ysize))
+            if self.glitch_size_range[0] <= 1.0 and isinstance(self.glitch_size_range[0], float):
+                glitch_size = random.randint(
+                    int(self.glitch_size_range[0] * ysize),
+                    int(self.glitch_size_range[1] * ysize),
+                )
             else:
                 glitch_size = random.randint(self.glitch_size_range[0], self.glitch_size_range[1])
-            
+
             # generate random direction
-            direction = random.choice([-1,1])
-            
-            # generate random glitch offset 
-            if self.glitch_offset_range[0] <=1.0 and isinstance(self.glitch_offset_range[0], float):
-                glitch_offset = random.randint(int(self.glitch_offset_range[0]*xsize), int(self.glitch_offset_range[1]*xsize)) * direction
+            direction = random.choice([-1, 1])
+
+            # generate random glitch offset
+            if self.glitch_offset_range[0] <= 1.0 and isinstance(self.glitch_offset_range[0], float):
+                glitch_offset = (
+                    random.randint(int(self.glitch_offset_range[0] * xsize), int(self.glitch_offset_range[1] * xsize))
+                    * direction
+                )
             else:
                 glitch_offset = random.randint(self.glitch_offset_range[0], self.glitch_offset_range[1]) * direction
-            
+
             # get a patch of image
             start_y = random.randint(0, ysize - glitch_size)
-            image_patch = image[start_y:start_y+glitch_size, :]
+            image_patch = image[start_y : start_y + glitch_size, :]
             pysize, pxsize = image_patch.shape[:2]
-            
+
             # create translation matrix in horizontal direction
             translation_matrix = np.float32([[1, 0, glitch_offset], [0, 1, 0]])
-            
+
             # get a copy of translated area
-            if direction>0:
+            if direction > 0:
                 image_patch_fill = image_patch[:, -glitch_offset:].copy()
-            else:   
+            else:
                 image_patch_fill = image_patch[:, :glitch_offset].copy()
             # translate image
             image_patch = cv2.warpAffine(image_patch, translation_matrix, (pxsize, pysize))
             # fill back the empty are after translation
-            if direction>0:
+            if direction > 0:
                 image_patch[:, :glitch_offset] = image_patch_fill
-            else:   
+            else:
                 image_patch[:, -glitch_offset:] = image_patch_fill
-                
+
             # randomly scale single channel to create a single color contrast effect
             random_ratio = random.uniform(0.8, 1.2)
-            channel = random.randint(0,2)
-            image_patch_ratio = image_patch[:,:,channel].astype("int") *  random_ratio
-            image_patch_ratio[image_patch_ratio>255]=255
-            image_patch_ratio[image_patch_ratio<0] = 0
-            image_patch[:,:,channel] = image_patch_ratio.astype("uint8")
-            
+            channel = random.randint(0, 2)
+            image_patch_ratio = image_patch[:, :, channel].astype("int") * random_ratio
+            image_patch_ratio[image_patch_ratio > 255] = 255
+            image_patch_ratio[image_patch_ratio < 0] = 0
+            image_patch[:, :, channel] = image_patch_ratio.astype("uint8")
 
-            image[start_y:start_y+glitch_size, :] = image_patch
-        
+            image[start_y : start_y + glitch_size, :] = image_patch
+
         return image
-        
+
     def __call__(self, image, layer=None, force=False):
         if force or self.should_run():
-            image = image.copy()      
-            
+            image = image.copy()
+
             # apply color shift before the glitch effect
-            color_shift = ColorShift(color_shift_offset_x_range = (3,5),
-                                     color_shift_offset_y_range = (3,5),
-                                     color_shift_iterations = (1,2),
-                                     color_shift_brightness_range = (0.9,1.1),
-                                     color_shift_gaussian_kernel_range = (1,3),
-                                     p=1)
+            color_shift = ColorShift(
+                color_shift_offset_x_range=(3, 5),
+                color_shift_offset_y_range=(3, 5),
+                color_shift_iterations=(1, 2),
+                color_shift_brightness_range=(0.9, 1.1),
+                color_shift_gaussian_kernel_range=(1, 3),
+                p=1,
+            )
             image_output = color_shift(image)
 
             # check and generate random direction
@@ -125,19 +131,19 @@ class GlitchEffect(Augmentation):
                 glitch_direction = random.choice(["vertical", "horizontal"])
             else:
                 glitch_direction = self.glitch_direction
-            
+
             # for vertical direction, rotate image by 90 degree because apply_glitch create glithces horizontally
             if glitch_direction == "vertical":
-                image_output = np.rot90(self.apply_glitch(np.rot90(image_output,1)),3)
+                image_output = np.rot90(self.apply_glitch(np.rot90(image_output, 1)), 3)
             elif glitch_direction == "horizontal":
-                image_output = self.apply_glitch(image_output)  
+                image_output = self.apply_glitch(image_output)
             # for 2 directional glitches, it will be either horizontal or vertical direction first
             else:
-                if random.random()>0.5:
-                    image_output = self.apply_glitch(image_output)  
-                    image_output = np.rot90(self.apply_glitch(np.rot90(image_output,1)),3)
+                if random.random() > 0.5:
+                    image_output = self.apply_glitch(image_output)
+                    image_output = np.rot90(self.apply_glitch(np.rot90(image_output, 1)), 3)
                 else:
-                    image_output = np.rot90(self.apply_glitch(np.rot90(image_output,1)),3)
-                    image_output = self.apply_glitch(image_output) 
-                    
+                    image_output = np.rot90(self.apply_glitch(np.rot90(image_output, 1)), 3)
+                    image_output = self.apply_glitch(image_output)
+
             return image_output
