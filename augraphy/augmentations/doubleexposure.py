4f36698b296390d83acb7ff30dd4386f0bb09607
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2023-12-07 17:18:25 +0800

    Rerun pre-commit.

--- augraphy/augmentations/doubleexposure.py
+++ augraphy/augmentations/doubleexposure.py
@@ -6,7 +6,6 @@ import numpy as np
 from augraphy.base.augmentation import Augmentation
 
 
-
 class DoubleExposure(Augmentation):
     """Emulates double exposure effect when taking picture with phone camera.
 
@@ -24,16 +23,15 @@ class DoubleExposure(Augmentation):
 
     def __init__(
         self,
-        gaussian_kernel_range=(9,12),
-        offset_direction=1,
-        offset_range=(18,25),
+        gaussian_kernel_range=(9, 12),
+        offset_direction="random",
+        offset_range=(18, 25),
         p=1,
     ):
         super().__init__(p=p)
         self.gaussian_kernel_range = gaussian_kernel_range
         self.offset_direction = offset_direction
         self.offset_range = offset_range
-         
 
     # Constructs a string representation of this Augmentation.
     def __repr__(self):
@@ -55,10 +53,9 @@ class DoubleExposure(Augmentation):
                 is_gray = 1
                 image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
-
             # generate random gaussian kernel
             gaussian_kernel = random.randint(self.gaussian_kernel_range[0], self.gaussian_kernel_range[1])
-             # kernel must be odd
+            # kernel must be odd
             if not gaussian_kernel % 2:
                 gaussian_kernel += 1
 
@@ -68,36 +65,36 @@ class DoubleExposure(Augmentation):
                 sign = 1
             else:
                 sign = -1
-            
+
             # update offset direction
             if self.offset_direction == "random":
-                offset_direction = random.choice([0,1])
+                offset_direction = random.choice([0, 1])
             else:
                 offset_direction = self.offset_direction
 
             # apply blur
-            image_blur = cv2.GaussianBlur(image, [gaussian_kernel,gaussian_kernel],0)
+            image_blur = cv2.GaussianBlur(image, [gaussian_kernel, gaussian_kernel], 0)
             image_output = image_blur.copy()
             if offset != 0:
-                for i in range(1, abs(offset)+1):
+                for i in range(1, abs(offset) + 1):
                     coffset = i
                     image_blur_offset = image_blur.copy()
                     # vertical
-                    if offset_direction:  
-                        if sign>0:
-                            image_blur_offset[coffset:,:] = image_blur_offset[:-coffset,:] 
+                    if offset_direction:
+                        if sign > 0:
+                            image_blur_offset[coffset:, :] = image_blur_offset[:-coffset, :]
                         else:
-                            image_blur_offset[:-coffset,:] = image_blur_offset[coffset:,:] 
-                   # horizontal
+                            image_blur_offset[:-coffset, :] = image_blur_offset[coffset:, :]
+                    # horizontal
                     else:
-                        if sign>0:
-                            image_blur_offset[:, coffset:] = image_blur_offset[:, :-coffset] 
+                        if sign > 0:
+                            image_blur_offset[:, coffset:] = image_blur_offset[:, :-coffset]
                         else:
-                            image_blur_offset[:, :-coffset] = image_blur_offset[:, coffset:] 
-                       
+                            image_blur_offset[:, :-coffset] = image_blur_offset[:, coffset:]
+
                     # blend blur image to output
                     image_output = cv2.addWeighted(image_output, 0.9, image_blur_offset, 0.1, 0)
-                        
+
             # return image follows the input image color channel
             if is_gray:
                 image_output = cv2.cvtColor(image_output, cv2.COLOR_BGR2GRAY)
